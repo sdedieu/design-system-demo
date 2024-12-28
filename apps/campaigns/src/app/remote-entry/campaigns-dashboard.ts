7d557ecf228def8c5f3f8ccb82765a56034cf834
Sylvain Dedieu <sylvaindedieu@mac.home>
2024-12-28 21:42:38 +0100

    feat(cds-styles): add tailwind

--- apps/campaigns/src/app/remote-entry/campaigns-dashboard.ts
+++ apps/campaigns/src/app/remote-entry/campaigns-dashboard.ts
@@ -5,7 +5,7 @@ import { CdsButtonModule } from "@cds-library";
     imports: [CdsButtonModule],
     selector: 'ds-cmp-campaigns-dashboard',
     template: `<h1>Campaigns</h1>
-    <div class="flex gap-1">
+    <div class="cds-flex cds-gap-1">
         <button mat-flat-button>Button</button>
         <button cds-size="small" mat-flat-button>Button</button>
     </div>
---
+++ libs/cds-styles/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: [
+    'display', 'gap', 'flex', 'flexBasis', 'flexDirection', 'flexGrow', 'flexShrink', 'flexWrap', 'alignItems'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: 'cds-',
+}
+
