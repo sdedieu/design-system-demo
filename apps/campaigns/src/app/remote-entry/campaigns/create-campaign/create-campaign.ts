5c3b47dced55f3bc8ce202be6495b894fcd4550a
s.dedieu <s.dedieu@criteo.com>
2026-05-28 09:13:08 +0200

    fix(dialog): fix layout

--- apps/campaigns/src/app/remote-entry/campaigns/create-campaign/create-campaign.ts
+++ apps/campaigns/src/app/remote-entry/campaigns/create-campaign/create-campaign.ts
@@ -5,6 +5,7 @@ import { CdsCardModule } from '@cds-library/card';
 import { CdsChipsModule } from '@cds-library/chips';
 import { CdsDividerModule } from '@cds-library/divider';
 import { CdsIconModule } from '@cds-library/icon';
+import { CdsDialogModule } from '@cds-library/dialog';
 
 interface CampaignTypeTag {
   icon: string;
@@ -20,58 +21,56 @@ interface CampaignTypeCard {
 }
 
 @Component({
-  imports: [CdsCardModule, CdsButtonModule, CdsChipsModule, CdsDividerModule, CdsIconModule],
+  imports: [CdsCardModule, CdsButtonModule, CdsChipsModule, CdsDialogModule, CdsDividerModule, CdsIconModule],
   selector: 'ds-cmp-create-campaign-dialog',
   template: `
-    <section>
-      <header class="cds-flex cds-justify-between cds-items-center cds-px-4 cds-py-2">
-        <strong>Campaign creation</strong>
+    <header class="cds-flex cds-justify-between cds-items-center cds-px-4 cds-py-2">
+      <strong>Campaign creation</strong>
 
-        <div class="cds-flex cds-items-center cds-gap-4">
-          <span>{{ advertiserName }}</span>
-          <button mat-icon-button aria-label="Close campaign creation" (click)="close()">
-            <mat-icon>close</mat-icon>
-          </button>
-        </div>
-      </header>
-
-      <mat-divider></mat-divider>
-
-      <div class="cds-px-4 cds-py-12">
-        <h1 class="cds-text-center cds-mb-12">What type of campaign are you launching?</h1>
-
-        <div class="cds-grid cds-grid-cols-3 cds-gap-4">
-          @for (card of cards; track card.name) {
-          <mat-card appearance="outlined">
-            <img mat-card-image class="cds-m-auto" height="320" width="400" [src]="card.img" [alt]="card.alt" />
-
-            <mat-card-content>
-              <strong>{{ card.name }}</strong>
-              <p>
-                {{ card.description }}
-                <a href="" (click)="$event.preventDefault()">Learn more</a>
-              </p>
-
-              <mat-chip-set [attr.aria-label]="card.name + ' highlights'">
-                @for (tag of card.tags; track tag.label) {
-                <mat-chip>
-                  <mat-icon matChipAvatar>{{ tag.icon }}</mat-icon>
-                  {{ tag.label }}
-                </mat-chip>
-                }
-              </mat-chip-set>
-            </mat-card-content>
-
-            <mat-divider></mat-divider>
-
-            <mat-card-actions align="end">
-              <button mat-flat-button color="primary" [attr.aria-label]="'Start ' + card.name" (click)="startCampaign()">Start</button>
-            </mat-card-actions>
-          </mat-card>
-          }
-        </div>
+      <div class="cds-flex cds-items-center cds-gap-4">
+        <span>{{ advertiserName }}</span>
+        <button mat-icon-button aria-label="Close campaign creation" (click)="close()">
+          <mat-icon>close</mat-icon>
+        </button>
       </div>
-    </section>
+    </header>
+
+    <mat-divider></mat-divider>
+
+    <mat-dialog-content>
+      <h1 class="cds-text-center cds-mb-12">What type of campaign are you launching?</h1>
+
+      <div class="cds-grid cds-grid-cols-3 sm:cds-gap-4 md:cds-gap-8">
+        @for (card of cards; track card.name) {
+        <mat-card appearance="outlined">
+          <img mat-card-image class="cds-m-auto" [style.aspect-ratio]="'1/1'" [style.max-height]="'250px'" [src]="card.img" [alt]="card.alt" />
+
+          <mat-card-content>
+            <strong>{{ card.name }}</strong>
+            <p>
+              {{ card.description }}
+              <a href="" (click)="$event.preventDefault()">Learn more</a>
+            </p>
+
+            <mat-chip-set [attr.aria-label]="card.name + ' highlights'">
+              @for (tag of card.tags; track tag.label) {
+              <mat-chip>
+                <mat-icon matChipAvatar>{{ tag.icon }}</mat-icon>
+                {{ tag.label }}
+              </mat-chip>
+              }
+            </mat-chip-set>
+          </mat-card-content>
+
+          <mat-divider></mat-divider>
+
+          <mat-card-actions align="end">
+            <button mat-flat-button color="primary" [attr.aria-label]="'Start ' + card.name" (click)="startCampaign()">Start</button>
+          </mat-card-actions>
+        </mat-card>
+        }
+      </div>
+    </mat-dialog-content>
   `,
 })
 export class CreateCampaignDialog {
